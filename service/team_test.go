2f9c37b94165a866db7bb92a44c7b39186dabe0c
Boy Sandy Gladies Arriezona <dev@renodesper.com>
2021-10-12 11:03:29 +0700

    Feat: Add integration test for CreateTeam

--- service/team_test.go
+++ service/team_test.go
@@ -2,7 +2,6 @@ package service
 
 import (
 	"context"
-	"encoding/json"
 	"testing"
 
 	"github.com/google/uuid"
@@ -11,63 +10,61 @@ import (
 
 func TestGetAllTeams(t *testing.T) {
 	cases := map[string]struct {
-		SortBy           string
-		Sort             string
-		Skip             int
-		Limit            int
-		ExpectedError    bool
-		ExpectedResponse string
+		SortBy        string
+		Sort          string
+		Skip          int
+		Limit         int
+		ExpectedError bool
 	}{
 		"SuccessGetAllTeams": {
-			SortBy:           "created_at",
-			Sort:             "DESC",
-			Skip:             0,
-			Limit:            10,
-			ExpectedError:    false,
-			ExpectedResponse: "[{\"id\":\"933efe12-2219-42df-bd51-a2e84888432d\",\"name\":\"team1\",\"isDeleted\":false,\"createdAt\":\"2021-10-10T16:57:23.672096Z\",\"updatedAt\":\"0001-01-01T00:00:00Z\"}]",
+			SortBy:        "created_at",
+			Sort:          "DESC",
+			Skip:          0,
+			Limit:         10,
+			ExpectedError: false,
 		},
 		"FailedOffsetMustNotBeNegative": {
-			SortBy:           "created_at",
-			Sort:             "DESC",
-			Skip:             -1,
-			Limit:            0,
-			ExpectedError:    true,
-			ExpectedResponse: "null",
+			SortBy:        "created_at",
+			Sort:          "DESC",
+			Skip:          -1,
+			Limit:         0,
+			ExpectedError: true,
 		},
 		"FailedLimitMustNotBeNegative": {
-			SortBy:           "created_at",
-			Sort:             "DESC",
-			Skip:             0,
-			Limit:            -1,
-			ExpectedError:    true,
-			ExpectedResponse: "null",
+			SortBy:        "created_at",
+			Sort:          "DESC",
+			Skip:          0,
+			Limit:         -1,
+			ExpectedError: true,
 		},
 		"FailedEmptyResponse": {
-			SortBy:           "created_at",
-			Sort:             "DESC",
-			Skip:             100000,
-			Limit:            0,
-			ExpectedError:    false,
-			ExpectedResponse: "[]",
+			SortBy:        "created_at",
+			Sort:          "DESC",
+			Skip:          100000,
+			Limit:         0,
+			ExpectedError: false,
 		},
 	}
 
-	for v, test := range cases {
+	for v, tc := range cases {
 		t.Run(v, func(t *testing.T) {
-
 			ctx := context.Background()
 			svc := NewTeamService(Log, DB)
 
-			teams, err := svc.GetAllTeams(ctx, test.SortBy, test.Sort, test.Skip, test.Limit)
+			teams, err := svc.GetAllTeams(ctx, tc.SortBy, tc.Sort, tc.Skip, tc.Limit)
 
-			if test.ExpectedError {
+			if tc.ExpectedError {
 				assert.Error(t, err)
+				assert.Empty(t, teams)
 			} else {
 				assert.NoError(t, err)
-			}
 
-			b, _ := json.Marshal(teams)
-			assert.Equal(t, test.ExpectedResponse, string(b))
+				if v == "FailedEmptyResponse" {
+					assert.NotNil(t, teams)
+				} else {
+					assert.NotEmpty(t, teams)
+				}
+			}
 		})
 	}
 }
@@ -79,36 +76,69 @@ func TestGetTeam(t *testing.T) {
 		ExpectedResponse string
 	}{
 		"SuccessGetTeam": {
-			ID:               "933efe12-2219-42df-bd51-a2e84888432d",
-			ExpectedError:    false,
-			ExpectedResponse: "{\"id\":\"933efe12-2219-42df-bd51-a2e84888432d\",\"name\":\"team1\",\"isDeleted\":false,\"createdAt\":\"2021-10-10T16:57:23.672096Z\",\"updatedAt\":\"0001-01-01T00:00:00Z\"}",
+			ID:            "933efe12-2219-42df-bd51-a2e84888432d",
+			ExpectedError: false,
 		},
 		"FailedGetTeamNotFound": {
-			ID:               "933efe12-2219-42df-bd51-a2e84888432e",
-			ExpectedError:    true,
-			ExpectedResponse: "null",
+			ID:            "933efe12-2219-42df-bd51-a2e84888432e",
+			ExpectedError: true,
 		},
 	}
 
-	for v, test := range cases {
+	for v, tc := range cases {
 		t.Run(v, func(t *testing.T) {
-
 			ctx := context.Background()
 			svc := NewTeamService(Log, DB)
 
-			id, err := uuid.Parse(test.ID)
+			id, err := uuid.Parse(tc.ID)
 			assert.NoError(t, err)
 
 			team, err := svc.GetTeam(ctx, id)
 
-			if test.ExpectedError {
+			if tc.ExpectedError {
 				assert.Error(t, err)
+				assert.Nil(t, team)
 			} else {
 				assert.NoError(t, err)
+				assert.NotNil(t, team)
 			}
+		})
+	}
+}
 
-			b, _ := json.Marshal(team)
-			assert.Equal(t, test.ExpectedResponse, string(b))
+func TestCreateTeam(t *testing.T) {
+	cases := map[string]struct {
+		Name             string
+		ExpectedError    bool
+		ExpectedResponse string
+	}{
+		"SuccessCreateTeam": {
+			Name:          "team100",
+			ExpectedError: false,
+		},
+		"FailedTeamExist": {
+			Name:          "team100",
+			ExpectedError: true,
+		},
+	}
+
+	for v, tc := range cases {
+		t.Run(v, func(t *testing.T) {
+			ctx := context.Background()
+			svc := NewTeamService(Log, DB)
+
+			payload := CreateTeamRequest{
+				Name: tc.Name,
+			}
+			team, err := svc.CreateTeam(ctx, &payload)
+
+			if tc.ExpectedError {
+				assert.Error(t, err)
+				assert.Empty(t, team)
+			} else {
+				assert.NoError(t, err)
+				assert.NotEmpty(t, team)
+			}
 		})
 	}
 }
