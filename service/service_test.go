a194a91a059ac9bdd24a0307b71ebd4ca09c819c
Boy Sandy Gladies Arriezona <dev@renodesper.com>
2021-10-12 13:10:21 +0700

    Feat: Add golang-migrate for testing

--- service/service_test.go
+++ service/service_test.go
@@ -2,10 +2,14 @@ package service
 
 import (
 	"flag"
+	"log"
 	"os"
 	"testing"
 
 	"github.com/go-pg/pg/v10"
+	"github.com/golang-migrate/migrate/v4"
+	_ "github.com/golang-migrate/migrate/v4/database/postgres"
+	_ "github.com/golang-migrate/migrate/v4/source/file"
 	"github.com/spf13/viper"
 	"gitlab.com/renodesper/spenmo-test/repository/postgre"
 	"gitlab.com/renodesper/spenmo-test/util/logger"
@@ -15,6 +19,7 @@ import (
 
 var Log *logger.Loggers
 var DB *pg.DB
+var M *migrate.Migrate
 
 func TestMain(m *testing.M) {
 	loadConfig()
@@ -51,5 +56,22 @@ func initService() {
 	} else {
 		Log = logger.New(noop.CreateLogger())
 		DB = postgre.NewPostgreClient(Log, dbUsername, dbPassword, dbHost, dbPort, dbName)
+	}
+
+	var err error
+	M, err = migrate.New(
+		"file://../config/db/migrations",
+		"postgres://user:password@127.0.0.1:5432/spenmo?sslmode=disable")
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func reinitializeDB() {
+	if err := M.Down(); err != nil {
+		log.Fatal(err)
+	}
+	if err := M.Up(); err != nil {
+		log.Fatal(err)
 	}
 }
